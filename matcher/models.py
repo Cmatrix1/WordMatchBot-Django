f5957aa811be0003b0ab1a254ef042c5cd815457
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-12 01:05:29 +0330

    make better the models

--- matcher/models.py
+++ matcher/models.py
@@ -8,10 +8,7 @@ class OrCondition(models.Model):
     ]
     keywords = models.CharField(max_length=255)
     response = models.ForeignKey("matcher.MessageResponse", verbose_name="", on_delete=models.CASCADE, related_name="or_conditions")
-    operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="operation")
-    
-    or_operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="or_operation", help_text='At least one condition must be met to send the message.')
-    and_operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='or', related_name="and_operation", help_text='All conditions must pass for the message to be sent.')
+    operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="operation", help_text='At least one condition must be met to send the message.')
 
     def __str__(self):
         return f"[{self.keywords}] {self.operation} {self.response}"
@@ -26,12 +23,9 @@ class AndCondition(models.Model):
         ('or', 'Or'),
     ]
     keywords = models.CharField(max_length=255)
-    response = models.ForeignKey("matcher.MessageResponse", verbose_name="", on_delete=models.CASCADE, related_name="and_conditions")
+    response = models.ForeignKey("matcher.MessageResponse", verbose_name="", on_delete=models.CASCADE, related_name="and_conditions", help_text='All conditions must pass for the message to be sent.')
     operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="operation")
     
-    or_operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="or_operation", help_text='At least one condition must be met to send the message.')
-    and_operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='or', related_name="and_operation", help_text='All conditions must pass for the message to be sent.')
-
     def __str__(self):
         return f"[{self.keywords}] {self.operation} {self.response}"
     
