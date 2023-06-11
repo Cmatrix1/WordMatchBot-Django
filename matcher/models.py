cd53deeb53992c8836b1670fd29f26e44f7be2b5
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-12 02:14:47 +0330

    update the models and make better the realtions

--- matcher/models.py
+++ matcher/models.py
@@ -1,31 +1,14 @@
 from django.db import models
 
 
-class OrCondition(models.Model):
+class Condition(models.Model):
     KEYWORD_OPERATION = [
         ('and', 'And'),
         ('or', 'Or'),
     ]
     keywords = models.CharField(max_length=255)
-    response = models.ForeignKey("matcher.MessageResponse", verbose_name="", on_delete=models.CASCADE, related_name="or_conditions")
-    operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="operation", help_text='At least one condition must be met to send the message.')
+    operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and')
 
-    def __str__(self):
-        return f"[{self.keywords}] {self.operation} {self.response}"
-    
-    def get_keywords(self):
-        return self.keywords.split(',')
-
-
-class AndCondition(models.Model):
-    KEYWORD_OPERATION = [
-        ('and', 'And'),
-        ('or', 'Or'),
-    ]
-    keywords = models.CharField(max_length=255)
-    response = models.ForeignKey("matcher.MessageResponse", verbose_name="", on_delete=models.CASCADE, related_name="and_conditions", help_text='All conditions must pass for the message to be sent.')
-    operation = models.CharField(max_length=5, choices=KEYWORD_OPERATION, default='and', related_name="operation")
-    
     def __str__(self):
         return f"[{self.keywords}] {self.operation} {self.response}"
     
@@ -35,4 +18,6 @@ class AndCondition(models.Model):
 
 class MessageResponse(models.Model):    
     response = models.TextField()
+    or_condition = models.ManyToManyField(Condition, related_name="or_condition", help_text='At least one condition must be met to send the message.')
+    and_condition = models.ManyToManyField(Condition, related_name="and_condition", help_text='All conditions must pass for the message to be sent.')
 
